1d537033748f99b2996b464456ad175ed4cb10af
jheg <jason@jheg.co.uk>
2015-03-22 12:12:58 +0000

    adding ajax

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,7 @@
 class BookmarksController < ApplicationController
   def index
   end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 root to: 'bookmarks#index'
+
+get '/show', to: 'bookmarks#show' 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
