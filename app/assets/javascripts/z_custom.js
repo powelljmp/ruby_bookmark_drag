c99d08593e1204b79995e8d924703b588981838d
jheg <jason@jheg.co.uk>
2015-03-22 15:00:36 +0000

    added bm form template

--- app/assets/javascripts/z_custom.js
+++ app/assets/javascripts/z_custom.js
@@ -8,18 +8,18 @@ $( document ).ready(function() {
 		grid: [ 10, 10 ]
 	});
 	// draggie.on( 'dragEnd', onDragEnd ); //add event listener
-  // $.ajax({
-  //     type:"GET",
-  //     url:"/show",
-  //     // data: {id: id, url:url, title:title, x:xx, y:yy, layer:layer, tags:tags },
-  //     // dataType: "json",
-  //     // datatype:"jsondata",
-  //     success:function(response){
-  //       $('#results').append(response)
-  //     }, complete : function(){
-  //       alert("finished!")
-  //     }   
-  //   });
+  $.ajax({
+      type:"GET",
+      url:"/show",
+      // data: {id: id, url:url, title:title, x:xx, y:yy, layer:layer, tags:tags },
+      // dataType: "json",
+      // datatype:"jsondata",
+      success:function(response){
+        $('#results').append(response)
+      }, complete : function(){
+        alert("finished!")
+      }   
+    });
 });	
 	
 //}
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,8 +3,27 @@ class BookmarksController < ApplicationController
     @bookmarks = Bookmark.all
   end
 
-  def show
+  def new 
+    @bookmark = Bookmark.new
   end
 
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+    flash[:notice] = "Bookmark added!"
+    redirect_to root_path
+  end
+
+
+
+  def show
+    render partial: 'shared/bookmark', layout: false
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit!
+  end
 
 end
