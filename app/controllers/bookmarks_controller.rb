f0dadd42ca19e0ef4fbfb44deffda5a34232b999
jheg <jason@jheg.co.uk>
2015-03-22 14:18:32 +0000

    bookmark mig

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,10 @@
 class BookmarksController < ApplicationController
   def index
+    @bookmarks = Bookmark.all
   end
 
   def show
   end
+
+
 end
---
+++ db/migrate/20150322124851_bookmark.rb
@@ -0,0 +1,10 @@
+class Bookmark < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.string :title
+      t.string :url
+      t.integer :category_id
+      t.timestamps
+    end
+  end
+end
