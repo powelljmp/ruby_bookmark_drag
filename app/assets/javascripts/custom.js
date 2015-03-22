5acebfb14156072b1c41de6cd308e44163ca8a9d
jheg <jason@jheg.co.uk>
2015-03-22 11:29:56 +0000

    removed old custom file

--- app/assets/javascripts/custom.js
+++
@@ -1,8 +0,0 @@
-//function addDraggieListener(newID){
-console.log("Add listener to ");
-	var elemA = document.querySelector("#basic");
-	var draggie = new Draggabilly( elemA, {
-		grid: [ 10, 10 ]
-	});
-	draggie.on( 'dragEnd', onDragEnd ); //add event listener
-//}
