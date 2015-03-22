ccd8f9c206f2a97a33c1d8a2314b20b968669530
james <powell.jmp@gmail.com>
2015-03-22 11:33:12 +0000

    JS loading on don.ready

--- app/assets/javascripts/z_custom.js
+++ app/assets/javascripts/z_custom.js
@@ -1,8 +1,15 @@
 //function addDraggieListener(newID){
-console.log("Add listener to ");
+$( document ).ready(function() {
+	
+	
+	console.log("Add listener to ");
 	var elemA = document.querySelector("#basic");
 	var draggie = new Draggabilly( elemA, {
 		grid: [ 10, 10 ]
 	});
 	draggie.on( 'dragEnd', onDragEnd ); //add event listener
+	
+	
+});	
+	
 //}
